ed3555a29339661794df6909badcc9b4673e82e5
nbouteme <nbouteme@users.noreply.github.com>
2019-10-24 15:29:54 +0200

    Initial data structures

---
+++ includes/board.h
@@ -0,0 +1,27 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+typedef struct {
+	int active : 1;
+	int flagged : 1;
+	int n : 6;
+} __attribute__((packed)) cell_t;
+
+typedef union {
+	struct {
+		int x, y;
+	};
+	struct {
+		int w, h;
+	};
+} point_t;
+
+typedef point_t dim_t;
+
+typedef struct {
+	cell_t *board;
+	point_t cursor;
+	dim_t dims;
+} board_t;
+
+#endif /* BOARD_H */
