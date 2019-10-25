e6ccd9bcb1beb3768b4fb409c353f51619d17e70
gglaive <gglaive@rutherfordium.mime.univ-paris8.fr>
2019-10-25 10:32:12 +0200

    conflicts

---
+++ src/affichage.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../includes/board.h"
+
+// Une ligne qui contient le nombre de mines présentes
+// le nombre de flags placés
+// au milieu de la ligne le smiley windows 95 
+// afficher tout le jeu (2 boucles for)
+// case non active = case x, si flag = drapeau genre p, si active = affiche contenu (0= espace, sinon chiffre)
+
+
+////////////////////////////////////////
+// FONCTION AFFICHER LA GRILLE DE JEU //
+////////////////////////////////////////
+void AfficheJeu(board_t* board)
+{
+    printf("\n\n");
+    // Une ligne qui contient le nombre de mines présentes
+	printf(" mines : %d	",10);
+	// au milieu de la ligne le smiley windows 95
+	printf("	:)		");
+	// le nombre de flags placés
+	printf(" drapeaux placés : %d	\n",0);
+
+    for(int i=0;i<16;i++)
+		{
+		for(int j=0;j<30;j++)
+			{                
+				printf("x");
+			}
+				printf("\n");
+		}
+}
+
+int main()
+{     
+	struct board_t board;
+	(*board).cursor = null;
+	(*board).dims = null;
+	AfficheJeu(&board);
+	return 0;
+}
